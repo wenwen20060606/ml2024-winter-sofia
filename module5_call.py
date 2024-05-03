0f2d80deb586956708767179249ffc8442d0daf4
wenwen20060606 <167954036+wenwen20060606@users.noreply.github.com>
2024-05-03 14:39:24 -0700

    Add files via upload

---
+++ module5_call.py
@@ -0,0 +1,4 @@
+number = input()
+number._init_(5)
+number.input_list(5)
+number.output(5, 10)
---
+++ module5_mod.py
@@ -0,0 +1,18 @@
+class input()
+    def _init_(self,N)
+        self.N = input("please type a positiev integer:\"}
+        
+    def input_list(self,N)
+        input_list = []
+        for index in range(N):
+            input_list.append(input("Please type in a number:\n"))
+    
+    def output(self,X,N)
+    X = input("Please type an integer:\n"}
+    found = False
+    for index in range(N):
+        if X == input_list[index]:
+            print(index+1)
+            found = True
+    if not found:
+        print(-1)            
